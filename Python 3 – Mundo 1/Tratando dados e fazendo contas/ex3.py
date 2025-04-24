8eebe3f95df40162ebc620ea762ee0872e136da8
Gabriel Santos <gabriel.santos@amf.edu.br>
2025-04-24 16:40:20 -0300

    EX3

---
+++ Python 3 – Mundo 1/Tratando dados e fazendo contas/ex3.py
@@ -0,0 +1,3 @@
+primeiro_numero = int(input("Primeiro número: "))
+segundo_numero = int(input("Segundo número: "))
+print(f"A soma de {primeiro_numero} + {segundo_numero} = {(primeiro_numero+segundo_numero)}")
